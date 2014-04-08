cea7a3fa466c7b33104ce2447e8ffa9b480d6e36
Jacob Carey <jacob.carey@marriott.com>
2014-04-08 15:59:43 -0400

    Add myls.c

---
+++ chap_1/myls.c
@@ -0,0 +1,21 @@
+#include <dirent.h>
+
+int main(int argc, char* argv[])
+{
+	DIR* dp;
+	struct dirent* dirp;
+
+	if (argc != 2)
+		fprintf(stderr, "usage: ls directory_name");
+		return 1;
+
+	if ((dp = opendir(argv[1])) == NULL)
+		fprintf(stderr, "can't open %s", argv[1]);
+
+	while ((dirp = readdir(dp)) != NULL)
+		printf("%s\n", dirp->d_name);
+
+	closedir(dp);
+
+	return 0;
+}
