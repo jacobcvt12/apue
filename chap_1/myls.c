a267b14b1d3d0f4c57d8ab0b1a695f040400fd06
Jacob Carey <jacobcvt12@gmail.com>
2014-04-08 20:24:01 -0400

    Fix myls.c

--- chap_1/myls.c
+++ chap_1/myls.c
@@ -1,16 +1,23 @@
+#include <stdio.h>
 #include <dirent.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[])
 {
-	DIR* dp;
-	struct dirent* dirp;
+	DIR *dp;
+	struct dirent *dirp;
 
 	if (argc != 2)
-		fprintf(stderr, "usage: ls directory_name");
+    {
+		fprintf(stderr, "usage: ls directory_name\n");
 		return 1;
+    }
 
 	if ((dp = opendir(argv[1])) == NULL)
-		fprintf(stderr, "can't open %s", argv[1]);
+    {
+		fprintf(stderr, "can't open %s\n", argv[1]);
+        return 1;
+    }
 
 	while ((dirp = readdir(dp)) != NULL)
 		printf("%s\n", dirp->d_name);
