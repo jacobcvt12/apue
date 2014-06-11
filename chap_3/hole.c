7e93db3a6d2927f24dcf8ca61f749992df1c2075
Jacob Carey <jacobcvt12@gmail.com>
2014-06-10 23:38:31 -0400

    Work on section 3.6

--- chap_3/hole.c
+++ chap_3/hole.c
@@ -2,17 +2,23 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-char buf1[] = "abcdefghij";
-char buf2[] = "ABCDEFGHIJ";
+char    buf1[] = "abcdefghij";
+char    buf2[] = "ABCDEFGHIJ";
 
 int main(void)
 {
-    int fd;
+    int     fd;
 
     if ((fd = creat("file.hole", FILE_MODE)) < 0)
     {
-        printf("creat error\n");
+        fprintf(stderr, "creat error");
         return 1;
     }
 
-if 
+    if (write(fd, buf1, 10) != 10)
+    {
+        fprintf(stderr, "buf1 write error");
+        return 1;
+    }
+    /* offset now = 10 */
+
